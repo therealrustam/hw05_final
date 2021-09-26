017726103359a21c5efd9a09f0310143cdcbd090
therealrustam <therealrustam@yandex.com>
2021-09-26 16:31:10 +0500

    Спринт 6 Вахитов Р Версия - 6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -141,10 +141,7 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if request.user.id != author.id:
-        Follow.objects.get_or_create(user=request.user, author=author)
-    else:
-        return redirect('posts:profile', username)
+    Follow.objects.get_or_create(user=request.user, author=author)
     return redirect('posts:follow_index')
 
 
